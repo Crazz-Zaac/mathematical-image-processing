88ac327e39b1db3e1b616c191f92123dedb550f9
crazz-zaac <r.alpha919@gmail.com>
2024-05-27 21:23:13 +0200

    fixed title

--- mip/display/image.py
+++ mip/display/image.py
@@ -23,26 +23,28 @@ def read(path: str, cmap: str) -> np.ndarray:
 
 
 def subplot_images(images: List[np.ndarray],
-                   titles: Optional[List[str]], fig_size: tuple = (10, 10),
+                   titles: Optional[List[str]] = None, fig_size: tuple = (20, 10), dpi=100,
                    cmap: str = 'gray', order: Tuple[int, int] = (1, -1)):
     """
     Subplot multiple images using matplotlib.
-
     """
     order = (order[0], len(images) // order[0]) if order[1] == -1 else \
-        (len(images)//order[1], order[1]) if order[0] == -1 else order
-
-    fig, axes = plt.subplots(order[0], order[1], figsize=fig_size)
+            (len(images) // order[1], order[1]) if order[0] == -1 else order
+    # print(order)
+    fig, axes = plt.subplots(order[0], order[1], figsize=fig_size, dpi=dpi)
+    # plt.subplots_adjust(wspace=0, hspace=0.1)  # Adjust the spacing between subplots
+    plt.tight_layout()
     for i, ax in enumerate(axes.flat):
         if i < len(images):
             ax.imshow(images[i], cmap=cmap)
             ax.axis('off')
             if titles:
-                ax.set_title(titles[i])
-            else:
-                ax.set_title(f'Image {i+1}')
+                ax.set_title(titles[i], fontsize=12)
+        else:
+            ax.axis('off')  # Turn off axes for empty subplots
 
     plt.show()
+    return fig
 
 
 def show(image: np.ndarray,
