c68779520453230976be810d69e9868b30328f5e
crazz-zaac <r.alpha919@gmail.com>
2024-05-27 21:23:47 +0200

    add the solution

--- mip/src/tykhonov_regularization.py
+++ mip/src/tykhonov_regularization.py
@@ -1,8 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from typing import Tuple
 
-def grad(I: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+def grad(I: np.ndarray) -> np.ndarray:
     """
     Computes the gradient of an image I.
     --------
@@ -10,26 +8,37 @@ def grad(I: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     I: np.ndarray
     --------
     Returns:
-    grad_x: np.ndarray    
+    G: np.ndarray
     """
-    I = np.array(I)
-    grad_x = np.gradient(I, axis=0)
-    grad_y = np.gradient(I, axis=1)
-    return grad_x, grad_y
+    M, N = I.shape
+    G = np.zeros((M, N, 2))
+    G[:, 1:, 0] = I[:, 1:] - I[:, :-1]
+    G[:, 0, 0] = I[:, 0]
+    G[1:, :, 1] = I[1:, :] - I[:-1, :]
+    G[0, :, 1] = I[0, :]
+    return G
 
-def div(grad_x: np.ndarray, grad_y: np.ndarray) -> np.ndarray:
+
+def div(G: np.ndarray) -> np.ndarray:
     """
     Computes the divergence of the gradient.
     --------
     Parameters:
-    grad_x: np.ndarray
-    grad_y: np.ndarray
+    G: np.ndarray
     --------
     Returns:
-    div: np.ndarray    
+    p: np.ndarray
     """
-    div = np.gradient(grad_x, axis=0) + np.gradient(grad_y, axis=1)
-    return div
+    M, N, _ = G.shape
+    divergence = np.zeros((M, N))
+    divergence[:, :-1] += G[:, :-1, 0]
+    divergence[:, 1:] -= G[:, :-1, 0]
+    divergence[:, 0] = -G[:, 0, 0]
+    divergence[:-1, :] += G[:-1, :, 1]
+    divergence[1:, :] -= G[:-1, :, 1]
+    divergence[0, :] = -G[0, :, 1]
+    return divergence
+
 
 def laplacian(I: np.ndarray) -> np.ndarray:
     """
@@ -41,6 +50,56 @@ def laplacian(I: np.ndarray) -> np.ndarray:
     Returns:
     lap: np.ndarray
     """
-    I = np.array(I)
-    lap = np.gradient(np.gradient(I, axis=0), axis=0) + np.gradient(np.gradient(I, axis=1), axis=1)
+    lap = div(grad(I))
     return lap
+
+
+# laplacian(np.random.randint(0, 255, (100, 100)))
+
+
+def tykohonov_gradient(
+    I: np.ndarray, lambda_: float, noisy_image: np.ndarray = None, tau: float = 0.1
+) -> np.ndarray:
+    """
+    Computes the Tykohonov gradient of an image I.
+    --------
+    Parameters:
+    I: np.ndarray
+    lambda_: float
+    noisy_image: np.ndarray
+    tau: float
+    --------
+    Returns:
+    tyk: np.ndarray
+    """
+    if noisy_image is None:
+        noisy_image = I.copy()
+
+    J_prime = I - noisy_image - lambda_ * laplacian(I)
+    tyk = I - tau * J_prime
+    return tyk
+
+
+if __name__ == "__main__":
+    from mip.display.image import read, subplot_images
+
+    img = read(r"../assets/cameraman_sp.png", "GRAY")
+    images = []
+    timg = img.copy()
+    taus = [0.01, 0.1, 1]
+    lambdas = [0.01, 0.1, 1]
+
+    titles = []
+    iterations = 120
+    every = 120
+    for tau in taus:
+        for lambda_ in lambdas:
+            for i in range(iterations + 1):
+                timg = tykohonov_gradient(timg, lambda_, img, tau)
+                if i > 0 and i % every == 0:
+                    images.append(timg)
+                    titles.append(f"λ={lambda_}, τ={tau}, iter={i}")
+
+    subplot_images(
+        images, titles=titles, fig_size=(20, 18), order=(-1, 3), dpi=100
+    ).savefig("tykohonov.png")
